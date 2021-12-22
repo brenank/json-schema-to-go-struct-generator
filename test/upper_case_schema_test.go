17f658b5ea6397ffbc004d3263ec99961f2e0dfe
Jonathan Langley <jono.langley@azarc.io>
2021-12-21 21:47:08 -0800

    Stop struct names being generated with all upper case when schema title's are all uppercase

---
+++ test/upper_case_schema_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"github.com/azarc-io/json-schema-to-go-struct-generator/pkg/converter"
+	"github.com/azarc-io/json-schema-to-go-struct-generator/test/generated/upper-case-titles/upper-case-schema"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+//go:generate go run ../cmd/main.go --input ./samples/upper-case-titles --output ./generated/upper-case-titles
+
+func TestConvert(t *testing.T) {
+	wd, _ := os.Getwd()
+	filePath := filepath.Join(wd, "samples/upper-case-titles/upper-case-schema.json")
+	err := converter.Convert([]string{filePath}, "./generated/upper-case-titles")
+	assert.Empty(t, err)
+}
+
+func TestHasLowerCaseOnUpperCaseTitles(t *testing.T) {
+	assert.NotNil(t, models.BarIt{})
+	assert.NotNil(t, models.SomeTest{})
+}
