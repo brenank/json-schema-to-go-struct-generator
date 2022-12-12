e9274dc7c1bc39da87776814c4b4cbbf791c15ce
Brenan Kelley <brenankly@gmail.com>
2022-12-12 13:50:29 -0800

    allow specifying package name

--- cmd/main.go
+++ cmd/main.go
@@ -15,13 +15,18 @@ func main() {
 		panic(err)
 	}
 
+	packageName := flags.PackageName
+	if packageName == "" {
+		packageName = "models"
+	}
+
 	outPath, err := filepath.Abs(flags.OutputPath)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("Processing files: %v\n", files)
-	err = converter.Convert(files, "models", outPath, false)
+	err = converter.Convert(files, packageName, outPath, false)
 
 	if err != nil {
 		panic(err)
